c61eff8b50141346701328effe78c27892beb9d3
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-15 16:39:26 -0600

    Clase 2 completadada

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const btn = document.querySelector('[data-form-btn]');
 
-console.log(btn);
-
-btn.addEventListener("click", function () {
+const createTask = (evento) => {
     const input = document.querySelector('[data-form-input]');
+    evento.preventDefault();
     console.log("Crear tarea");
     console.log(input.value);
-});
+}
+console.log(btn);
+
+//Arrow functions o funciones anonimas
+btn.addEventListener("click",createTask);
 
