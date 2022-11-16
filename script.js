faccd4e1943f11bdb94d657395552ba0a56bb17c
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-15 18:03:28 -0600

    Se agrego codigo para mostrar el div con el contenido del input

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
     const input = document.querySelector('[data-form-input]');
-    evento.preventDefault();
-    console.log("Crear tarea");
-    console.log(input.value);
+    evento.preventDefault(); //Para que no se reinicie la pagina cuando apriente el boton agregar
+    const value = input.value;
+    const task = document.querySelector("[data-task]");
+    input.value = ""; //para limpiar el input despues de enviar el texto
+    //backticks 
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    console.log(content);
 }
 console.log(btn);
 
