e90bda88bfda8a6b3fba7b2756ef5ff40d446cb3
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-16 15:54:37 -0600

    Se agregaron los modulos y se quito el IIFE

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-( () => {
+
+    import checkComplete from "./components/checkComplete.js";
+    import deleteIcon from "./components/deleteIcon.js";
+
+
     const btn = document.querySelector('[data-form-btn]');
     const createTask = (evento) => { 
         evento.preventDefault(); //Para que no se reinicie la pagina cuando apriente el boton agregar
@@ -23,29 +27,7 @@
 
     //Arrow functions o funciones anonimas
     btn.addEventListener("click",createTask);
-    const checkComplete = () => {
-        const i = document.createElement("i");
-        i.classList.add("far", "fa-check-square", "icon");
-        i.addEventListener("click", completeTask);
-        return i;
-    };
+    
+    
 
-    //Immediately invoked function expression IIFE
-    const completeTask = (event) => {
-        const element = event.target;
-        element.classList.toggle("fas"); //agrega fas la clase 
-        element.classList.toggle("completeIcon"); //togle verifica si exixte la clase si existe la quita y sino la pone
-        element.classList.toggle("far"); //elinima la clase 
-    };
-    const deleteIcon = () => {
-        const i = document.createElement("i");
-        i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
-        i.addEventListener('click',delateTask);
-        return i;
-    };
-    const delateTask = (event) => {
-        const parent = event.target.parentElement;
-        parent.remove();//remove() elmina todo lo que se le paso en parent
-    };
-})();
 
