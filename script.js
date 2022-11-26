13a607bfbb6dafdd0fd8912a3c5185e636e94f71
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-25 18:08:00 -0600

    Se agrego calnedar y date para obtener el valor de fechas

--- script.js
+++ script.js
@@ -7,7 +7,10 @@
     const createTask = (evento) => { 
         evento.preventDefault(); //Para que no se reinicie la pagina cuando apriente el boton agregar
         const input = document.querySelector('[data-form-input]');
+        const calendar = document.querySelector('[data-form-date]');
         const value = input.value;
+        const date = calendar.value; //para obtener contenido de calendar
+        console.log(date);
         const list = document.querySelector("[data-list]")
         const task = document.createElement("li");
         task.classList.add('card')
